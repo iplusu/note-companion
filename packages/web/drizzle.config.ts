28f3801b12e48545c8a47db93c5a8cbe0a708a84
iplusu <iplusu@gmail.com>
2025-10-18 07:19:51 -0400

    Update drizzle.config.ts

--- packages/web/drizzle.config.ts
+++ packages/web/drizzle.config.ts
@@ -5,18 +5,14 @@ const projectDir = process.cwd();
 loadEnvConfig(projectDir);
 
 export default defineConfig({
-  schema: "./drizzle/schema.ts",
+  schema: "./drizzle/schema.ts", // 사용자님의 경로를 따릅니다
   dialect: "postgresql",
   dbCredentials: {
-    // host: process.env.POSTGRES_HOST,
-    // user: process.env.POSTGRES_USER,
-    // password: process.env.POSTGRES_PASSWORD,
-    // database: process.env.POSTGRES_DATABASE,
-    host: "db.uityjashzcxhlvitislw.supabase.co",
+    host: process.env.POSTGRES_HOST!,
     port: 6543,
-    user: "postgres",
-    password: "XmBiPBx3hqIIgCs7",
-    database: "postgres",
+    user: process.env.POSTGRES_USER!,
+    password: process.env.POSTGRES_PASSWORD!,
+    database: process.env.POSTGRES_DATABASE!,
     ssl: {
       rejectUnauthorized: false
     }
