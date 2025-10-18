9006e203cc822e9750405a6dd23723971271d291
iplusu <iplusu@gmail.com>
2025-10-18 07:11:24 -0400

    Update drizzle.config.ts

--- packages/web/drizzle.config.ts
+++ packages/web/drizzle.config.ts
@@ -12,11 +12,11 @@ export default defineConfig({
     // user: process.env.POSTGRES_USER,
     // password: process.env.POSTGRES_PASSWORD,
     // database: process.env.POSTGRES_DATABASE,
-    host: process.env.POSTGRES_HOST!,
+    host: process.env.POSTGRES_HOST,
     port: 6543,
-    user: process.env.POSTGRES_USER!,
-    password: process.env.POSTGRES_PASSWORD!,
-    database: process.env.POSTGRES_DATABASE!,
+    user: process.env.POSTGRES_USER,
+    password: process.env.POSTGRES_PASSWORD,
+    database: process.env.POSTGRES_DATABASE,
     ssl: {
       rejectUnauthorized: false
     }
